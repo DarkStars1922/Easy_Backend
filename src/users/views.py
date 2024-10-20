8f3faa3e849dc418ff8807ab6f8fe024c68cbb0c
DarkStars1922 <the_mind_omega@outlook.com>
2024-10-20 16:12:43 +0545

    1.0 cookie debug

--- src/users/views.py
+++ src/users/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 
 # 获取用户
-def user_get(request):
+def get_user(request):
     username = request.COOKIES['username']
     if not username:
         return redirect('login')
