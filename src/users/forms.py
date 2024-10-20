6af0566540ee48f574e182753bc48f330ca9b78e
DarkStars1922 <the_mind_omega@outlook.com>
2024-10-20 12:46:22 +0545

    favorite debug 2.2

--- src/users/forms.py
+++ src/users/forms.py
@@ -19,7 +19,7 @@ class ArticleCreateForm(forms.ModelForm):
         model = Article
         fields = ['title','content']
         
-class FavoriteForm(forms.Form):
+class FavoriteForm(forms.ModelForm):
     class Meta:
         model = Favorite
         fields = ['user','article']
