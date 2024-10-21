00acd1068530b08e97a1dbb4e1681b715ef2a42d
DarkStars1922 <the_mind_omega@outlook.com>
2024-10-21 18:37:51 +0545

    comment debug 1.4

--- src/users/models.py
+++ src/users/models.py
@@ -35,7 +35,7 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('users:article_detail',args=[self.id])
+        return reverse('article_detail',args=[self.id])
 
 # 收藏夹类如下：
 class Favorite(models.Model):
