0340435c011720c377440670a53777b9ec4441ff
DarkStars1922 <the_mind_omega@outlook.com>
2024-10-21 18:34:01 +0545

    comment debug 1.2

--- src/comments/urls.py
+++ src/comments/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name='comments'
 urlpatterns=[
-    path('post_comment/<int:article_id>/',views.post_comment,name = 'comment'),
+    path('post_comment/<int:article_id>/',views.post_comment,name = 'post_comment'),
 ]
